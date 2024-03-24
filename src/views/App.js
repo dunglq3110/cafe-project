8fbb58845dfd2f12c75987d49231d8636176d6a9
vutientrinh <vutientrinh.0357@gmail.com>
2024-03-24 21:13:25 +0700

    signin vs setup lại appjs

--- src/views/App.js
+++ src/views/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ManagerPage from "./manager/ManagerPage";
+import SignInPage from "./login/signin";
 
 function App() {
     return (
-        <div className="App">
-            <ManagerPage/>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/signin" element={<SignInPage />} />
+                <Route path="/manager" element={<ManagerPage />} />
+            </Routes>
+        </Router>
     );
 }
 
