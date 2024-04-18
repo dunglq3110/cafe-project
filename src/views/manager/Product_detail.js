9ecadedee86c5583b8786006e4fc68a2d4d0e0d0
vutientrinh <vutientrinh.0357@gmail.com>
2024-04-18 20:28:26 +0700

    product-detail

--- src/views/manager/Product_detail.js
+++ src/views/manager/Product_detail.js
@@ -3,13 +3,9 @@ function Product_detail() {
     return (
         <div className="Product_detail">
             <div class="mx-3 bg-custom rounded">
-                <div class="row mx-2 my-2">
-                    <div class="col col-4 d-inline align-items-center justify-content-center mt-4">
-                        <img src={coffee} class="rounded mx-auto d-block w-75 h-75 img-thumbnail" />
-                        <div class="mt-4 d-flex justify-content-around">
-                            <button type="button" class="btn btn-danger">Remove</button>
-                            <button type="button" class="btn btn-info">Edit</button>
-                        </div>
+                <div class="row mx-2 my-3">
+                    <div class="col col-4 d-inline align-items-center justify-content-center mt-4 mb-4">
+                        <img src={coffee} class="rounded mx-auto d-block w-75 h-100 img-thumbnail" />
                     </div>
                     <div class="col col-8">
                         <div class="row">
@@ -20,42 +16,37 @@ function Product_detail() {
                                     <label for="inputName" class="form-label">Name</label>
                                     <input type="text" class="form-control" id="inputName" />
                                 </div>
-                                <div class="col-md-8 mt-2">
-                                    <label for="inputEmail" class="form-label">Email</label>
-                                    <input type="email" class="form-control" id="inputEmail4" />
-                                </div>
-                                <div class="col-md-4 mt-2">
-                                    <label for="inputState" class="form-label">Gentle</label>
-                                    <select id="inputState" class="form-select">
-                                        <option selected>Male</option>
-                                        <option>Female</option>
-                                        <option>Other</option>
+                                <div class="col-md-6 mt-2">
+                                    <label for="inputType" class="form-label">Type</label>
+                                    <select id="inputType" class="form-select">
+                                        <option selected>Type 1</option>
+                                        <option>Type 2</option>
                                     </select>
                                 </div>
-
                                 <div class="col-md-6 mt-2">
-                                    <label for="inputCity" class="form-label">Phone Number</label>
-                                    <input type="tel" class="form-control" id="inputCity" />
-                                </div>
-                                <div class="col-md-4 mt-2">
-                                    <label for="inputState" class="form-label">State</label>
-                                    <select id="inputState" class="form-select">
+                                    <label for="inputStatus" class="form-label">Status</label>
+                                    <select id="inputStatus" class="form-select">
                                         <option selected>Active</option>
                                         <option>Inactive</option>
                                     </select>
                                 </div>
-                                <div class="col-md-2 mt-2">
-                                    <label for="inputZip" class="form-label">Role</label>
-                                    <input type="text" class="form-control" id="inputRole" />
+                                <div class="col-md-4 mt-2">
+                                    <label for="inputSize" class="form-label">Size</label>
+                                    <select id="inputSize" class="form-select">
+                                        <option selected>L</option>
+                                        <option>M</option>
+                                        <option>S</option>
+                                    </select>
                                 </div>
-                                <div class="col-12 mt-2">
-                                    <label for="inputAddress" class="form-label">Birthday</label>
-                                    <input type="date" class="form-control" id="inputBirthday" placeholder="" />
+                                <div class="col-md-8 mt-2">
+                                <label for="inputPrice" class="form-label">Price</label>
+                                    <input type="text" class="form-control" id="inputPrice" />
                                 </div>
-                                <div class="col-12 mt-2">
-                                    <label for="inputAddress" class="form-label">Address</label>
-                                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" />
-                                </div>
+                                <div class="mt-4 d-flex justify-content-around">
+                            <button type="button" class="btn btn-danger w-15">Remove</button>
+                            <button type="button" class="btn btn-info w-15">Edit</button>
+                        </div>
+
                             </form>
                         </div>
                     </div>
